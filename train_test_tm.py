d2c5f536d8ec6f0f605ac1ed860afed8721fb5cb
makrai <makraimarton@gmail.com>
2015-10-02 17:55:48 +0200

    fitted to the rest

--- train_test_tm.py
+++ train_test_tm.py
@@ -1,39 +1,41 @@
 import argparse
-import logging
+import os
 
 from dinu14.train_tm import train_wrapper
-from dinu14.test_tm import test_wrapper
+from dinu14.test_tm import MxTester
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('seed')
-    parser.add_argument('source')
-    parser.add_argument('target')
+    parser.add_argument('seed_fn')
+    parser.add_argument('source_fn')
+    parser.add_argument('target_fn')
     parser.add_argument('--reverse', action='store_true')
     parser.add_argument('--additional', type=int)
-    parser.add_argument('--mx')
-    parser.add_argument('--test_from_line', type=int)
-    parser.add_argument('--train_size', type=int)
+    parser.add_argument('--mx_fn')
+    parser.add_argument('--test_from_line', type=int, default=None)
+    parser.add_argument('--train_size', type=int, default=5000)
     return parser.parse_args()
 
 
 def train_test_wrapper(args):
-    if bool(args['mx']) != bool(args['test_from_line']):
-        logging.warning(
-            'In order to test an existing translation matrix, both --mx\
-            and --test_from_line are needed')
-    elif args['mx'] and args['test_from_line']:
-        return test_wrapper(args['mx'], args['seed'], args['source'],
-                            args['target'], args['additional'],
-                            reverse=args['reverse'],
-                            first_test=args['test_from_line'])
-    mx, last_train = train_wrapper(args['seed'], args['source'],
-                                   args['target'], reverse=args['reverse'],
-                                   mx_fn=args['mx'], train_size=args['train_size'])
-    return test_wrapper(mx, args['seed'], args['source'], args['target'],
-                        args['additional'], reverse=args['reverse'],
-                        first_test=last_train+1)
+    if args.mx_fn and os.path.isfile(args.mx_fn):
+        if args.test_from_line is None:
+            raise Exception(
+                'In order to test an existing translation matrix, both' +
+                '--mx_fn and --test_from_line are needed')
+        elif hasattr(args, 'mx_fn'):
+            args.mx_fn = '{}.txt'.format(args.mx_fn)
+            return MxTester(args).test_wrapper()
+    else:
+        mx_fn, last_train = train_wrapper(
+            args.seed_fn, args.source_fn, args.target_fn,
+            reverse=args.reverse, mx_fn=args.mx_fn,
+            train_size=args.train_size)
+        if hasattr(args, 'mx_fn'):
+            args.mx_fn = '{}.txt'.format(args.mx_fn)
+        args.test_from_line = last_train + 1
+        return MxTester(args).test_wrapper()
 
 
 if __name__ == '__main__':
-    train_test_wrapper(vars(parse_args()))
+    train_test_wrapper(parse_args())
