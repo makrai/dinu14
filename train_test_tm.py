f97a0424d9460ace0e78fcf5b27474b958edd1b7
makrai <makraimarton@gmail.com>
2015-11-11 15:53:17 +0100

    end-to-end logging

--- train_test_tm.py
+++ train_test_tm.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from dinu14.train_tm import train_wrapper
-from dinu14.test_tm import MxTester
+from dinu14.test_tm import MxTester, get_logger
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -15,25 +15,23 @@ def parse_args():
     parser.add_argument('--mx_fn', help='including extension')
     parser.add_argument('--test_from_line', type=int, default=None)
     parser.add_argument('--train_size', type=int, default=5000)
+    parser.add_argument('--log-file', dest='log_fn') 
     return parser.parse_args()
 
 
 def train_test_wrapper(args):
+    get_logger(args.log_fn)
     if 'mx_fn' not in args:
         args.mx_fn = None
-    if args.mx_fn and os.path.isfile(args.mx_fn):
-        if args.test_from_line is None:
-            raise Exception(
-                'In order to test an existing translation matrix, both' +
-                '--mx_fn and --test_from_line are needed')
-        else: 
-            return MxTester(args).test_wrapper()
+    if args.mx_fn and os.path.isfile(args.mx_fn) and args.test_from_line:
+        return MxTester(args).test_wrapper()
     else:
-        mx, last_train = train_wrapper(
-            args.seed_fn, args.source_fn, args.target_fn,
-            reverse=args.reverse, mx_fn=args.mx_fn,
-            train_size=args.train_size)
+        mx, last_train = train_wrapper(args.seed_fn, args.source_fn,
+                                       args.target_fn, reverse=args.reverse,
+                                       mx_fn=args.mx_fn,
+                                       train_size=args.train_size)
         args.test_from_line = last_train + 1
+        args.mx_fn = None
         return MxTester(args, tr_mx=mx).test_wrapper()
 
 
