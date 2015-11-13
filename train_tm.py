35e2e765d7ed531a004fad2df861d2ac97e3f8bf
makrai <makraimarton@gmail.com>
2015-11-13 14:48:53 +0100

    seed translations used in train are excluded from test

--- train_tm.py
+++ train_tm.py
@@ -24,38 +24,37 @@ def parse_args():
     parser.add_argument(
         'target_fn',
         help="vectors in target language")
-    parser.add_argument('--reverse', action='store_true') 
+    parser.add_argument('--reverse', action='store_true')
     return parser.parse_args()
 
 
 def train_wrapper(seed_fn, source_fn, target_fn, reverse=False, mx_fn=None,
                   train_size=5000):
-    format_ = "%(asctime)s %(module)s (%(lineno)s) %(levelname)s %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=format_)
-
     train_data = read_dict(seed_fn, reverse=reverse)
 
     #we only need to load the vectors for the words in the training data
     #semantic spaces contain additional words
     source_words, target_words = zip(*train_data)
 
-    source_sp = Space.build(source_fn, set(source_words))
+    source_sp = Space.build(source_fn, lexicon=set(source_words))
     source_sp.normalize()
 
-    target_sp = Space.build(target_fn, set(target_words))
+    target_sp = Space.build(target_fn, lexicon=set(target_words))
     target_sp.normalize()
 
     logging.info("Learning the translation matrix")
-    tm, last_train = train_tm(source_sp, target_sp, train_data, train_size)
+    tm, used_for_train = train_tm(source_sp, target_sp, train_data, train_size)
 
     if mx_fn:
         logging.info("Saving the translation matrix to {}".format(mx_fn))
         np.save(mx_fn, tm)
 
-    return tm, last_train
+    return tm, used_for_train
 
 
 if __name__ == '__main__':
-    args = parse_args() 
+    args = parse_args()
+    format_ = "%(asctime)s %(module)s (%(lineno)s) %(levelname)s %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=format_)
     train_wrapper(args.seed_fn, args.source_fn, args.target_fn,
                   reverse=args.reverse, mx_fn=args.mx_fn)
